fe970e1e7afba238e6b43d2de70a75f93e99c082
RAJ SHUBHAM <59823172+Raj619Shubham@users.noreply.github.com>
2021-06-17 13:01:12 +0530

    Add files via upload
    Covid Live tracker global

---
+++ build/precache-manifest.bfe026d581333f6ad9d70838588e7565.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874c49c1f21f784cbb82d3afd1f34acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5afafad9ed361e50062b",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "bbeaf32f3b2551336ff9",
+    "url": "/static/css/main.c0db069c.chunk.css"
+  },
+  {
+    "revision": "5afafad9ed361e50062b",
+    "url": "/static/js/2.b5a3de4d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.b5a3de4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbeaf32f3b2551336ff9",
+    "url": "/static/js/main.9a223b23.chunk.js"
+  },
+  {
+    "revision": "83daad553a4b5ee78f26",
+    "url": "/static/js/runtime-main.61930f78.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
